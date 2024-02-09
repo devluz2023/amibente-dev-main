90e9912ed2cfded9000d537c5fcd8f9bb7f90ab7
binhojulix <fabio.jdluz@gmail.com>
2024-02-09 18:14:56 -0300

    ajustes

--- dev-nodejs/app.js
+++ dev-nodejs/app.js
@@ -1,16 +1,11 @@
-// Importing the built-in HTTP module
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3001
 
-// Creating a server object
-const server = http.createServer((req, res) => {
-  // Set the response HTTP header with HTTP status and Content type
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-  // Send the response body "Hello, World!"
-  res.end('Hello, World!\n');
-});
-
-// Server listens on port 3000 for incoming requests
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
